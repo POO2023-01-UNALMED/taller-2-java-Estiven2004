907e8f35fb49471d60244119d3859abf4f853958
Estiven2004 <eospinaj@unal.edu.co>
2023-02-20 23:05:37 -0500

    Update Asiento.java

--- Asiento.java
+++ Asiento.java
@@ -8,7 +8,6 @@ public class Asiento {
     void cambiarColor(String color){
         if (color == "rojo" || color =="verde" || color == "amarillo" || color =="negro" || color =="blanco"){
             this.color = color;
-        }
-    }  
+    }
+}  
 }
-    
